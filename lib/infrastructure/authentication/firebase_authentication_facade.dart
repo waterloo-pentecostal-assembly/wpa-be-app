79ff03462bc99cd2795e234233808d4fe8c8908b
Joshua Ramsamooj <jramsamooj@opentext.com>
2020-08-30 19:26:59 -0400

    Removed google sign in/up boilerplate

--- lib/infrastructure/authentication/firebase_authentication_facade.dart
+++ lib/infrastructure/authentication/firebase_authentication_facade.dart
@@ -1,23 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:wpa_app/domain/common/exceptions.dart';
 
 import '../../domain/authentication/entities.dart';
 import '../../domain/authentication/exceptions.dart';
 import '../../domain/authentication/interfaces.dart';
 import '../../domain/authentication/value_objects.dart';
+import '../../domain/common/exceptions.dart';
+import 'firebase_user_mapper.dart';
 
 class FirebaseAuthenticationFacade implements IAuthenticationFacade {
   final FirebaseAuth _firebaseAuth;
-  final GoogleSignIn _googleSignIn;
   final FirebaseUserMapper _firebaseUserMapper;
   final FirebaseFirestore _firestore;
 
   FirebaseAuthenticationFacade(
     this._firebaseAuth,
-    this._googleSignIn,
     this._firebaseUserMapper,
     this._firestore,
   );
@@ -28,9 +26,9 @@ class FirebaseAuthenticationFacade implements IAuthenticationFacade {
     User user;
 
     try {
-      user = await _firebaseAuth.currentUser;
+      user = _firebaseAuth.currentUser;
     } catch (e) {
-      throw UnexpectedError(e.toString());
+      throw UnexpectedError(message: e.toString());
     }
 
     if (user == null) {
@@ -41,32 +39,31 @@ class FirebaseAuthenticationFacade implements IAuthenticationFacade {
       String userId = user.uid;
       DocumentSnapshot userInfo =
           await _firestore.collection("users").doc(userId).get();
-      return _firebaseUserMapper.toDomain(userInfo.data(), userId);
+      return _firebaseUserMapper.toDomain(
+        userInfo.data(),
+        userId,
+      );
     } catch (e) {
-      throw UnexpectedError(e.toString());
+      throw UnexpectedError(message: e.toString());
     }
   }
 
   @override
-  Future registerWithEmailAndPassword(
-      {FirstName firstName,
-      LastName lastName,
-      EmailAddress emailAddress,
-      Password password}) {
+  Future registerWithEmailAndPassword({
+    FirstName firstName,
+    LastName lastName,
+    EmailAddress emailAddress,
+    Password password,
+  }) {
     // TODO: implement registerWithEmailAndPassword
     throw UnimplementedError();
   }
 
   @override
-  Future registerWithGoogle() {
-    // TODO: implement registerWithGoogle
-    throw UnimplementedError();
-  }
-
-  @override
-  Future signInWithEmailAndPassword(
-      {EmailAddress emailAddress, Password password}) async {
-
+  Future signInWithEmailAndPassword({
+    EmailAddress emailAddress,
+    Password password,
+  }) async {
     final String _emailAddress = emailAddress.value;
     final String _password = password.value;
 
@@ -87,7 +84,7 @@ class FirebaseAuthenticationFacade implements IAuthenticationFacade {
         ERROR_OPERATION_NOT_ALLOWED
       */
       if (e.code == 'ERROR_INVALID_EMAIL' || e.code == 'ERROR_WRONG_PASSWORD') {
-        // for security purposes, do not disclose specifically which is incorrect
+        // For security purposes, do not disclose specifically which is incorrect.
         throw InvalidEmailOrPassword();
       } else if (e.code == 'ERROR_USER_NOT_FOUND') {
         throw UserNotFound();
@@ -100,26 +97,7 @@ class FirebaseAuthenticationFacade implements IAuthenticationFacade {
   }
 
   @override
-  Future signInWithGoogle() {
-    throw UnimplementedError();
-  }
-
-  @override
   Future<void> signOut() {
-    return Future.wait([
-      // add sign out calls for other providers here if ever implemented.
-      _firebaseAuth.signOut(),
-    ]);
-  }
-}
-
-class FirebaseUserMapper {
-  LocalUser toDomain(Map<String, dynamic> userInfo, String userId) {
-    return LocalUser(
-      id: userId,
-      firstName: userInfo['first_name'],
-      lastName: userInfo['last_name'],
-      email: userInfo['email'],
-    );
+    return _firebaseAuth.signOut();
   }
 }
--- lib/injection.dart
+++ lib/injection.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 import 'application/authentication/authentication_bloc.dart';
 import 'application/authentication/sign_in/sign_in_bloc.dart';
@@ -10,6 +9,7 @@ import 'application/navigation_bar/navigation_bar_bloc.dart';
 import 'domain/authentication/interfaces.dart';
 import 'domain/bible_series/interfaces.dart';
 import 'infrastructure/authentication/firebase_authentication_facade.dart';
+import 'infrastructure/authentication/firebase_user_mapper.dart';
 import 'infrastructure/bible_series/bible_series_repository.dart';
 import 'presentation/phone/common/factories/text_factory.dart';
 
@@ -19,14 +19,14 @@ GetIt getIt = GetIt.instance;
 void init() {
   // Dependency Injection Configuration
 
-  // NOTE Different settings for different environments (PROD, TEST, etc) can be configured here as well
+  // NOTE: Different settings for different environments (PROD, TEST, etc) can be configured here as well
 
   /*
+  NOTE:
   Lazy Singleton - Class instance only created when it is first needed
   Factory - New instance each time 
   */
 
-  getIt.registerLazySingleton<GoogleSignIn>(() => GoogleSignIn());
   getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
   getIt.registerLazySingleton<FirebaseUserMapper>(() => FirebaseUserMapper());
   getIt.registerLazySingleton<FirebaseFirestore>(
@@ -53,7 +53,6 @@ void init() {
   getIt.registerLazySingleton<IAuthenticationFacade>(
     () => FirebaseAuthenticationFacade(
       getIt<FirebaseAuth>(),
-      getIt<GoogleSignIn>(),
       getIt<FirebaseUserMapper>(),
       getIt<FirebaseFirestore>(),
     ),
