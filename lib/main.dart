89da8afca1319cfd3d1696d37318e896a4f56445
Eliseo Nathaniel Ruiz Nowell <nathanielruiz98@gmail.com>
2020-09-13 21:22:28 -0700

    Add necessary plist files and project configurations to make ios app run in xcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
 
 import 'injection.dart';
 import 'presentation/app.dart';
 
-void main() {
+void main() async {
   init();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(App());
 }
