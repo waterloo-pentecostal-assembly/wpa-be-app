f98f4bbbc5b85bba5a55c4ff6e4f1a5d3a2ef311
Eliseo Nathaniel Ruiz Nowell <nathanielruiz98@gmail.com>
2020-09-14 21:49:48 -0700

    Update ios bundle id and remove firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-// Import the firebase_core plugin
-import 'package:firebase_core/firebase_core.dart';
 
 import 'injection.dart';
 import 'presentation/app.dart';
 
 void main() async {
   init();
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(App());
 }
