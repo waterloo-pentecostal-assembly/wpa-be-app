0c7205a3116f9453cd181cf54ba9ea5724303fd9
Joshua Ramsamooj <jramsamooj@opentext.com>
2020-09-18 23:40:53 -0400

    (WBA-11) Created entities for holding bible series and series content information

--- lib/domain/bible_series/entities.dart
+++ lib/domain/bible_series/entities.dart
@@ -1,8 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:wpa_app/domain/common/value_objects.dart';
 
-// TODO: create field to content info
+import '../common/value_objects.dart';
+
+enum SeriesContentType {
+  REFLECT,
+  LISTEN,
+  SCRIBE,
+  DRAW,
+  READ,
+  PRAY,
+  DEVOTIONAL,
+  MEMORIZE,
+}
+
+enum SeriesContentBodyType {
+  AUDIO,
+  TEXT,
+  SCRIPTURE,
+  QUESTION,
+}
+
 class BibleSeries {
   final UniqueId id;
   final String title;
@@ -10,71 +28,146 @@ class BibleSeries {
   final String imageUrl;
   final Timestamp startDate;
   final Timestamp endDate;
+  final bool isActive;
+  final List<SeriesContentSnippet> seriesContentSnippet;
 
   BibleSeries({
     @required this.id,
     @required this.title,
     @required this.subTitle,
     @required this.imageUrl,
-    @required @required this.startDate,
+    @required this.startDate,
     @required this.endDate,
+    @required this.isActive,
+    @required this.seriesContentSnippet,
   });
 
   @override
   String toString() {
-    return 'id: ${id.value}, title: $title, subtitle: $subTitle, imageUrl: $imageUrl, startDate: $startDate, endDate: $endDate';
+    return '''id: ${id.value}, title: $title, subtitle: $subTitle, imageUrl: $imageUrl, 
+              startDate: $startDate, endDate: $endDate, isActive: $isActive, 
+              seriesContentSnippet: $seriesContentSnippet''';
   }
 }
 
-// class BibleSeriesDetail {
-//   final UniqueId id;
-//   final String title;
-//   final String subTitle;
-//   final String imageUrl;
-//   final Timestamp startDate;
-//   final Timestamp endDate;
-//   final Map<String, Timestamp> contentIds;
+class SeriesContentSnippet {
+  final Map<SeriesContentType, UniqueId> contentTypes;
+  final Timestamp date;
 
-//   BibleSeriesDetail(
-//     this.id,
-//     this.title,
-//     this.subTitle,
-//     this.imageUrl,
-//     this.startDate,
-//     this.endDate,
-//     this.contentIds,
-//   );
-// }
+  SeriesContentSnippet({
+    @required this.contentTypes,
+    @required this.date,
+  });
 
-// class SeriesContent {
-//   final String id;
-//   final Map body;
-//   final Timestamp date;
-//   final String footer;
-//   final String title;
-//   final String subTitle;
-//   final SeriesContentType type;
-// }
-
-// entity for scriptures
-// TODO: restructure how data is represented in firestore
-class ScriptureEngagement {}
-
-// entity for audio
-class AudioEngagement {}
-
-// entity for text
-class TextEngagement {}
-
-enum SeriesContentType {
-  read,
-  listen,
-  pray,
-  reflect,
+  @override
+  String toString() {
+    return 'contentTypes: $contentTypes, date: $date';
+  }
 }
 
-enum SeriesContentBodyType {
-  scripture,
-  audio,
-  text,
+class SeriesContent {
+  final UniqueId id;
+  final SeriesContentType contentType;
+  final Timestamp date;
+  final String title;
+  final String subTitle;
+  final List<ISeriesContentBody> body;
+
+  SeriesContent({
+    @required this.id,
+    @required this.contentType,
+    @required this.date,
+    @required this.title,
+    @required this.subTitle,
+    @required this.body,
+  });
+
+  @override
+  String toString() {
+    return '''
+    id: $id, contentType: $contentType, date: $date, title: $title, subTitle: $subTitle, body: $body
+    ''';
+  }
+}
+
+class ISeriesContentBody {
+  final SeriesContentBodyType type;
+  final dynamic properties;
+
+  ISeriesContentBody({
+    @required this.type,
+    @required this.properties,
+  });
+}
+
+class AudioBody implements ISeriesContentBody {
+  final SeriesContentBodyType type;
+  final AudioBodyProperties properties;
+
+  AudioBody({
+    @required this.type,
+    @required this.properties,
+  });
+}
+
+class AudioBodyProperties {
+  String audioFileUrl;
+}
+
+class TextBody implements ISeriesContentBody {
+  final SeriesContentBodyType type;
+  final TextBodyProperties properties;
+
+  TextBody({
+    @required this.type,
+    @required this.properties,
+  });
+}
+
+class TextBodyProperties {
+  List<String> paragraphs;
+}
+
+class ScriptureBody implements ISeriesContentBody {
+  final SeriesContentBodyType type;
+  final ScriptureBodyProperties properties;
+
+  ScriptureBody({
+    @required this.type,
+    @required this.properties,
+  });
+}
+
+class ScriptureBodyProperties {
+  String bibleVersion;
+  String attribution;
+  List<Scripture> scriptures;
+}
+
+class Scripture {
+  final String book;
+  final String chapter;
+  final String title;
+  final Map<String, String> verses;
+
+  Scripture({
+    @required this.book,
+    @required this.chapter,
+    this.title,
+    @required this.verses,
+  });
+}
+
+class QuestionBody implements ISeriesContentBody {
+  final SeriesContentBodyType type;
+  final QuestionBodyProperties properties;
+
+  QuestionBody({
+    @required this.type,
+    @required this.properties,
+  });
+}
+
+class QuestionBodyProperties {
+  List<String> questions;
 }
