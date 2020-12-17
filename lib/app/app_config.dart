27e7287a6504cbd16fa5428e277d39321b02d28d
Joshua Ramsamooj <jramsamooj@Joshuas-MacBook-Pro.local>
2020-12-16 19:12:00 -0500

    added docs

--- lib/app/app_config.dart
+++ lib/app/app_config.dart
@@ -11,3 +11,6 @@ class AppConfig {
     @required this.title,
   });
 }
+
+// Roughly followed multienv setup: 
+// https://medium.com/@animeshjain/build-flavors-in-flutter-android-and-ios-with-different-firebase-projects-per-flavor-27c5c5dac10b
