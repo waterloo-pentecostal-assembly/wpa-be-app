a465c424d3368ecbc33300522c6ee6d45e0f8628
YiJie (Jackie) Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-01-12 13:53:08 -0500

    finished audio player functions

--- lib/presentation/engage/bible_series/widgets/audio_body.dart
+++ lib/presentation/engage/bible_series/widgets/audio_body.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../domain/bible_series/entities.dart';
@@ -10,17 +11,22 @@ class AudioContentBodyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AudioSlider();
+    return AudioSlider(audioContentBody: audioContentBody);
   }
 }
 
 class AudioSlider extends StatefulWidget {
+  final AudioBody audioContentBody;
+  const AudioSlider({Key key, this.audioContentBody}) : super(key: key);
   @override
   _AudioSliderState createState() => _AudioSliderState();
 }
 
 class _AudioSliderState extends State<AudioSlider> {
   bool playing = false;
+  AudioPlayer audioPlayer = new AudioPlayer();
+  Duration duration = new Duration();
+  Duration position = new Duration();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -33,9 +39,9 @@ class _AudioSliderState extends State<AudioSlider> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             mainAxisSize: MainAxisSize.max,
             children: [
-              Text("0:00"),
+              Container(width: 50, child: Text(_printDuration(position))),
               slider(),
-              Text("5:20"),
+              Container(width: 50, child: Text(_printDuration(duration))),
             ],
           )),
           InkWell(
@@ -54,17 +60,36 @@ class _AudioSliderState extends State<AudioSlider> {
     );
   }
 
-  //replace this method with bloc event add once audio player is implemented
-  void getAudio() {
+  void getAudio() async {
+    var url = widget.audioContentBody.properties.audioFileUrl;
     if (playing) {
+      var res = await audioPlayer.pause();
+      if (res == 1) {
+        setState(() {
+          playing = false;
+        });
+      }
       setState(() {
         playing = false;
       });
     } else {
-      setState(() {
-        playing = true;
-      });
+      var res = await audioPlayer.play(url, isLocal: true);
+      if (res == 1) {
+        setState(() {
+          playing = true;
+        });
+      }
     }
+    audioPlayer.onDurationChanged.listen((Duration d) {
+      setState(() {
+        duration = d;
+      });
+    });
+    audioPlayer.onAudioPositionChanged.listen((Duration d) {
+      setState(() {
+        position = d;
+      });
+    });
   }
 
   Widget slider() {
@@ -72,12 +97,21 @@ class _AudioSliderState extends State<AudioSlider> {
       child: Slider.adaptive(
           activeColor: Colors.blue,
           inactiveColor: Colors.grey,
-          min: 0.0, //changed later
-          value: 10, //changed later
-          max: 100, //changed later
+          min: 0.0,
+          value: position.inSeconds.toDouble(),
+          max: duration.inSeconds.toDouble(),
           onChanged: (double value) {
-            setState(() {}); //change to using bloc once audio player is decided
+            setState(() {
+              audioPlayer.seek(new Duration(seconds: value.toInt()));
+            });
           }),
     );
+  }
+
+  String _printDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$twoDigitMinutes:$twoDigitSeconds";
   }
 }
