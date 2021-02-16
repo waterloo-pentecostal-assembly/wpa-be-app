746d212053a0d5cfa1696e0f878f1dbdb66f7b9c
YiJie (Jackie) Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-02-16 12:37:55 -0500

    added privacy info and term of use page

--- lib/presentation/profile/privacy_policy.dart
+++ lib/presentation/profile/privacy_policy.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wpa_app/app/injection.dart';
+import 'package:wpa_app/presentation/common/text_factory.dart';
+import 'package:wpa_app/presentation/profile/helper.dart';
 
 class PrivacyPolicyPage extends StatelessWidget {
   @override
@@ -7,13 +10,11 @@ class PrivacyPolicyPage extends StatelessWidget {
       child: Container(
         child: Column(
           children: <Widget>[
-            Text('Privacy Policy'),
-            RaisedButton(
-              child: Text('Back'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )
+            HeaderWidget(title: 'Privacy Policy'),
+            Container(
+                padding: const EdgeInsets.all(24),
+                child: getIt<TextFactory>().lite(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet pulvinar odio. Duis ultrices in lacus at faucibus. Quisque quis nibh felis. Duis a est nec risus commodo elementum. Nam ut quam a diam posuere efficitur nec in urna. Praesent vulputate enim non enim luctus luctus. Quisque et ultrices sapien.'))
           ],
         ),
       ),
