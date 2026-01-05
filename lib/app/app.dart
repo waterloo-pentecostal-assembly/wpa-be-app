fb4d055c4fc8a8d9358c263b21015b19b88a50c6
Joshua Ramsamooj <joshua.ramsamooj@gmail.com>
2026-01-05 11:05:54 -0500

    more package updates

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,6 +20,10 @@ import 'injection.dart';
 class App extends StatelessWidget {
   Future<void> initializeServices() async {
     await Firebase.initializeApp();
+    await FirebaseAppCheck.instance.activate(
+      providerAndroid: const AndroidDebugProvider(),
+      providerApple: const AppleDebugProvider(),
+    );
     await getIt<FirebaseMessagingService>().initialize();
     FirebaseMessaging.instance
         .getInitialMessage()
